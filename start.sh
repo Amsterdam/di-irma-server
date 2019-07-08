58b963c922bbe2b4441ae103bab70348e0ece913
Edwin Martin <edwin@bitstorm.org>
2019-07-08 12:06:43 +0200

    Simplify Docker run command

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 docker stop irma_container
-docker rm irma_container
-docker run -it -p8088:8088 --name irma_container irma
+docker run -it --rm -p8088:8088 --name irma_container irma
