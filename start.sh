209a52eb03b022c1ae49182c47134afbb1057ee2
Tom Demeyer <tomd@xs4all.nl>
2020-03-04 11:39:08 +0100

    sync master met accept

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 docker stop irma_container
-docker build -t irma .
-# docker run -it --rm --env BASE_URL="https://attr.auth.amsterdam.nl" -p8088:8088 --name irma_container irma
 docker run -it --rm --env BASE_URL="https://acc.attr.auth.amsterdam.nl" -p8088:8088 --name irma_container irma
